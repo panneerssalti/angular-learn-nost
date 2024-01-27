d109d63c8ff0c3077c6ed51b450be79b7419f9ae
Panneer Selvam Shanmugam <panneerss.altimterik@gmail.com>
2024-01-27 16:49:20 +0530

    adding child and parent

--- src/app/parent-child-com/child-com/child-com.component.ts
+++ src/app/parent-child-com/child-com/child-com.component.ts
@@ -1,17 +1,50 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child-com',
   templateUrl: './child-com.component.html',
   styleUrl: './child-com.component.css'
 })
-export class ChildComComponent {
+export class ChildComComponent implements OnChanges {
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }
   @Input('feed') inputFeed = '';
   @Output() outputEvent = new EventEmitter<string>();
+  @Output() output = new EventEmitter<string>();
 
   emitEvent():void{
     let newDate:Date = new Date();
     let result: string = newDate.toLocaleString();
     this.outputEvent.emit(result);
   }
+
+  emitEventSec():void{
+    this.outputEvent.emit(this.generateRandomString(10));
+  }
+
+  generateRandomString(length: number): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+  
+    return result;
+  }
+
+  @Input() customValue: string = '';
+
+  // Output event
+  @Output() customValueChange = new EventEmitter<string>();
+
+  // Event handler for input changes
+  onInput(event: any) {
+    this.customValue = event.target.value;
+    this.customValueChange.emit(this.customValue);
+  }
+
+
 }
--- src/app/parent-child-com/parent-com/parent-com.component.ts
+++ src/app/parent-child-com/parent-com/parent-com.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent-com',
   templateUrl: './parent-com.component.html',
   styleUrl: './parent-com.component.css'
 })
-export class ParentComComponent {
+export class ParentComComponent implements OnDestroy,OnInit {
+  updateInterval: any;
+  ngOnInit(): void {
+    this.updateChildFeeder();
+    this.updateInterval = setInterval(() => {
+      this.updateChildFeeder();
+    }, 10000);
+  }
+  ngOnDestroy(): void {
+   clearInterval(this.updateInterval);
+  }
   dateArray:string[] = [];
 addDateString($event: string) {
   this.dateArray.push($event);
@@ -13,4 +23,25 @@ addDateString($event: string) {
 
   childFeeder:string = 'this is an example feeder';
 
+  updateChildFeeder():void{
+    const now = new Date();
+    this.childFeeder = now.toLocaleTimeString();
+  }
+
+  secondChildFeeder:string = "second feeder heelo";
+
+  parentValue: string = this.generateRandomString(10);
+
+
+  generateRandomString(length: number): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+  
+    return result;
+  }
 }
